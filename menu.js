3d9d935cfcce0078c63b8ea48243060c27cebee7
Matirg26 <matirg1807@gmail.com>
2024-12-09 21:55:53 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,6 +1,8 @@
-const menuToggle = document.getElementById('menu-toggle');
-const navMenu = document.getElementById('nav-menu');
+document.addEventListener("DOMContentLoaded", () => {
+    const menuToggle = document.getElementById("menu-toggle");
+    const navMenu = document.getElementById("nav-menu");
 
-menuToggle.addEventListener('click', () => {
-    navMenu.classList.toggle('active'); 
+    menuToggle.addEventListener("click", () => {
+        navMenu.classList.toggle("active");
+    });
 });
