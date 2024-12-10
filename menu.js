593b726736b376c3eba7619f125c9f434b744cf0
Matirg26 <matirg1807@gmail.com>
2024-12-09 22:51:49 -0500

    Update menu.js

--- menu.js
+++ menu.js
@@ -2,6 +2,7 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.getElementById("menu-toggle");
     const navMenu = document.getElementById("nav-menu");
 
+    // Activar/desactivar el menú al tocar el botón
     menuToggle.addEventListener("click", () => {
         navMenu.classList.toggle("active");
     });
